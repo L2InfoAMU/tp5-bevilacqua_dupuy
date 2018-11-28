1f05f259a8f610c9d094333d70aaedb2c6880717
d16010869 <aureliendupuy5@gmail.com>
2018-11-28 10:41:48 +0100

    Creation class BruteRasterImage Creation constructeur public BruteRasterImage(Color color, int width, int height)

---
+++ src/main/java/image/BruteRasterImage.java
@@ -0,0 +1,17 @@
+package image;
+
+import java.awt.*;
+
+public class BruteRasterImage {
+
+
+    private final Color color;
+    private final int widht;
+    private final int height;
+
+    public BruteRasterImage(Color color, int width, int height){
+        this.color = color;
+        this.widht = width;
+        this.height = height;
+    }
+}
