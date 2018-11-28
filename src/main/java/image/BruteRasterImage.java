3c0caf5594eeb9559979fa8631db111bf050a96f
d16010869 <aureliendupuy5@gmail.com>
2018-11-28 11:26:37 +0100

    Creation constructeur BruteRasterImage(Color[][] colors)

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -2,16 +2,29 @@ package image;
 
 import java.awt.*;
 
+import static util.Matrices.*;
+
 public class BruteRasterImage {
 
 
-    private final Color color;
-    private final int widht;
-    private final int height;
+    private  Color color;
+    private  int widht;
+    private  int height;
+    private  Color[][] colors;
+
 
     public BruteRasterImage(Color color, int width, int height){
         this.color = color;
         this.widht = width;
         this.height = height;
+    }
+
+    public BruteRasterImage(Color[][] colors){
+        for (int x = 0; x < getRowCount(colors); x++) {
+            this.colors[x] = colors[x];
+            for (int y = 0; y < colors[x].length; y++) {
+                this.colors[x][y] = colors[x][y];
+            }
+        }
     }
 }
