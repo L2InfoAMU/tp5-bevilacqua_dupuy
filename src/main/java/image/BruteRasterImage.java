9a1b6172969f4293372856deb5b64bd9d5344867
d16010869 <aureliendupuy5@gmail.com>
2018-11-30 09:54:24 +0100

    création méthode  void createRepresentation()

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -31,7 +31,9 @@ public class BruteRasterImage {
         }
     }
 
-    public void createRepresentation() { }
+    public void createRepresentation() {
+        Color colors[][] = new Color[][] ;
+    }
 
     public void setPixelColor(Color color, int x, int y){
         colors[x][y] = color;
